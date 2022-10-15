3284cde3bd987161957a4141c90ca9708b0025d4
Peer Maute <peer.maute@haw-hamburg.de>
2022-10-15 10:28:34 +0200

    first commit

---
+++ src/main/java/maute/brothers/splitsquare/SplitSquareApplication.java
@@ -0,0 +1,13 @@
+package maute.brothers.splitsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitSquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitSquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maute/brothers/splitsquare/SplitSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package maute.brothers.splitsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitSquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
