bb6000ff815791ea3609e6b80a1724a56ca5dbd8
Sjiun <sune-maute@web.de>
2022-10-16 10:22:16 +0200

    created ng components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BillsComponent } from './bills/bills.component';
+import { BillListComponent } from './bills/bill-list/bill-list.component';
+import { BillItemComponent } from './bills/bill-list/bill-item/bill-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillsComponent,
+    BillListComponent,
+    BillItemComponent
   ],
   imports: [
     BrowserModule,
