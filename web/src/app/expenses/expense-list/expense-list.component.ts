2beee45f70ceb8b4bb0bdf2cfd8039b069ff3691
Sjiun <sune-maute@web.de>
2022-10-16 10:55:08 +0200

    added rudimentary expense setup

--- web/src/app/expenses/expense-list/expense-list.component.ts
+++ web/src/app/expenses/expense-list/expense-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Expense } from '../expense.model';
 
 @Component({
   selector: 'app-expense-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expense-list.component.scss'],
 })
 export class expenseListComponent implements OnInit {
+  expenses: Expense[] = [
+    new Expense('Einkauf', 12.49),
+    new Expense('Neue Skier', 129.99),
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
---
+++ web/src/app/expenses/expense.model.ts
@@ -0,0 +1,3 @@
+export class Expense {
+  constructor(public name: string, public amount: number) {}
+}
